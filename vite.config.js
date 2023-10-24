6e52eb8b3b95f07b4bae7374ba4a35e2b606187c
nassirou <nassirou9993@gmail.com>
2023-10-24 15:22:50 +0100

    the setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/intro-section-with-dropdown-navigation-main/",
   plugins: [react()],
 })
